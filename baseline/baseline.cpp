cc7556651c5603d97787975fd293ca55350ce309
Lucas Weitzendorf <lweitzendorf@student.ethz.ch>
2021-11-05 21:25:37 +0100

    Renamed A* implementation to baseline

--- baseline/baseline.cpp
+++ baseline/baseline.cpp
@@ -2,32 +2,32 @@
 #include "FileParser.h"
 
 #include <iostream>
-#include <cstdlib>
 
 int main(int argc, char const *argv[]) {
   if (argc != 4) {
-    std::cout << "Usage: ./a_star <graph_file> <start_node> <goal_node>" << std::endl;
+    std::cout << "Usage: ./baseline <graph_file> <source_node> <target_node>" << std::endl;
     return -1;
   }
 
   WeightedGraph G;
   DotParser(argv[1]).build_graph(G);
 
-  vertex_t start = strtol(argv[2], nullptr, 10);
-  vertex_t goal = strtol(argv[3], nullptr, 10);
+  vertex_t source = strtol(argv[2], nullptr, 10);
+  vertex_t target = strtol(argv[3], nullptr, 10);
 
   std::cout << argv[1] << std::endl;
   std::cout << G.num_vertices() << " vertices" << std::endl;
   std::cout << G.num_edges() << " edges" << std::endl;
-  std::cout << start << " -> " << goal << std::endl;
+  std::cout << source << " -> " << target << std::endl;
 
-  std::list<vertex_t> path = G.a_star_search(start, goal);
+  std::list<vertex_t> path = G.a_star_search(source, target);
 
   if (path.empty()) {
     std::cout << "No path found!";
   } else {
-    for (vertex_t v : path)
+    for (vertex_t v : path) {
       std::cout << v << " ";
+    }
   }
   std::cout << std::endl;
 
