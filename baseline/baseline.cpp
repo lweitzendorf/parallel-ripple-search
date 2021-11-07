7267bde63e83de3427ca5db653907f15a2bfec24
Lucas Weitzendorf <lweitzendorf@student.ethz.ch>
2021-11-07 16:11:07 +0100

    Timer class

--- baseline/baseline.cpp
+++ baseline/baseline.cpp
@@ -1,8 +1,8 @@
 #include "WeightedGraph.h"
 #include "FileParser.h"
+#include "Timer.h"
 
 #include <iostream>
-#include <chrono>
 
 int main(int argc, char const *argv[]) {
   if (argc != 4) {
@@ -10,26 +10,32 @@ int main(int argc, char const *argv[]) {
     return -1;
   }
 
+  Timer timer;
   WeightedGraph G;
-  if (!DotParser(argv[1]).build_graph(G)) {
+  std::cout << argv[1] << std::endl;
+
+  timer.start();
+  bool valid = DotParser(argv[1]).build_graph(G);
+  timer.stop();
+  std::cout << "Parsed in " << timer.get_ms() << " ms" << std::endl;
+
+  if (!valid) {
     std::cout << "Invalid file!" << std::endl;
     return -2;
   }
 
+  std::cout << std::endl;
+  std::cout << G.num_vertices() << " vertices" << std::endl;
+  std::cout << G.num_edges() << " edges" << std::endl << std::endl;
+
   vertex_t source = strtol(argv[2], nullptr, 10);
   vertex_t target = strtol(argv[3], nullptr, 10);
 
-  std::cout << argv[1] << std::endl;
-  std::cout << G.num_vertices() << " vertices" << std::endl;
-  std::cout << G.num_edges() << " edges" << std::endl;
   std::cout << source << " -> " << target << std::endl;
-
-  auto t1 = std::chrono::high_resolution_clock::now();
+  timer.start();
   std::list<vertex_t> path = G.a_star_search(source, target);
-  auto t2 = std::chrono::high_resolution_clock::now();
-
-  long runtime_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-  std::cout << "Took " << runtime_ms << " ms" << std::endl;
+  timer.stop();
+  std::cout << "Path found in " << timer.get_ms() << " ms" << std::endl;
 
   if (path.empty()) {
     std::cout << "No path found!";
