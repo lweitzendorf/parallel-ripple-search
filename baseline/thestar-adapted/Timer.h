b5ae31a70e5f5de1fd195dba3d735c498841ec62
ischi09 <ishaan.shamanna@gmail.com>
2021-11-12 11:52:23 +0100

    Working adaptation of a* with Bitmap and Timer for 20x20

---
+++ baseline/thestar-adapted/Timer.h
@@ -0,0 +1,17 @@
+#ifndef DPHPC_TIMER_H
+#define DPHPC_TIMER_H
+
+#include <chrono>
+
+class Timer {
+  private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> t1;
+    std::chrono::time_point<std::chrono::high_resolution_clock> t2;
+
+  public:
+    void start();
+    void stop();
+    long get_ms();
+};
+
+#endif //DPHPC_TIMER_H
