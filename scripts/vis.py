16d82a5b69c96e3eff7bc1bdcb36a5ce4a8a2664
gavinleroy <gavinleroy6@gmail.com>
2021-12-22 17:41:16 +0100

    tweaks

--- scripts/vis.py
+++ scripts/vis.py
@@ -7,23 +7,33 @@ import numpy as np
 import os
 import pickle
 from pylab import cm
-from statistics import median
+from statistics import median, mean
 import sys
 
 # Configurations
 
 colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple', 'tab:red', 'tab:olive', 'tab:cyan', 'tab:pink']
 fill_color = 'white'
+group_bounds = [50, 150, 500, 1000, 100000000000000]
 
 # Basic Parsing Utilities
 
 def lmap(f, ls):
     return list(map(f, ls))
 
+def parse_costs(fn):
+    costs = []
+    with open(fn, 'rb') as fd:
+        while not_at_end(fd):
+            line = fd.readline()
+            cost = int(line)
+            costs.append(cost)
+    return costs
+
 def not_at_end(fd):
     pos = fd.tell()
     l = fd.readline().strip()
-    if l is None or l == "" or l[0] == '#':
+    if l is None or len(l) == 0 or l[0] == '#':
         return False
     fd.seek(pos)
     return True
@@ -40,13 +50,13 @@ def parse_id_set(fd):
     times, ohds = [float(time)], [float(overhead)]
     while not_at_end(fd):
         pos = fd.tell()
-        id, time, overhead = fd.readline().split()
-        if id != initial_id:
+        new_id, time, overhead = fd.readline().split()
+        if new_id != initial_id:
             fd.seek(pos)
             break
         times.append(float(time))
         ohds.append(float(overhead))
-    return id, times, ohds
+    return initial_id, times, ohds
 
 def parse_all_sets(fd):
     dct = {}
@@ -107,7 +117,6 @@ def barplot(data_dicts, label_names, max_samples=100, title='Some Bar', oput_fn=
     plt.legend(custom_lines, labels, loc='upper right', prop={'size':7})
     plt.show()
 
-# FIXME improve the quality of graph
 def boxplot(data_dicts, label_names, max_samples=3, title='Some Boxplot', oput_fn=None):
     off = 0.2
     center = -0.2
@@ -140,6 +149,61 @@ def boxplot(data_dicts, label_names, max_samples=3, title='Some Boxplot', oput_f
     # ax.legend(custom_lines, ['Cold', 'Medium', 'Hot'])
     plt.show()
 
+def grouped_boxplot(data_dicts, costs, label_names, max_samples=3, title='Some Boxplot', oput_fn=None):
+
+    cost_idxs = np.argsort(costs)
+    def loop(dict):
+        list_of_medians = lmap(lambda t: median(t[0]), dict.values())
+        npa = np.array(list_of_medians)
+        i = 0
+        results = []
+        current = []
+        for j, cost in  enumerate(npa[cost_idxs]):
+            if cost < costs[i]:
+                current.append(cost)
+            else:
+                results.append(current)
+                current = []
+                i += 1
+        return np.mean(results, axis=1)
+
+    grouped_runtimes = np.array(lmap())
+
+    off = 0.2
+    center = -0.2
+    width = 0.15
+    labels = lmap(lambda s: s.split(".")[0], label_names)
+    custom_lines = [Line2D([0], [0], color=color, lw=2) for color in colors]
+
+    indices = np.arange(len(data_dict))
+    values = np.array(lmap(lambda t: median(t[0]), data_dict.values()))
+    values = np.transpose(values)
+    plt.bar(indices + idx * width, values, width=width)
+
+    for idx, data_dict in enumerate(data_dicts):
+        times = np.array(lmap(lambda p: p[0], data_dict.values()))
+        times = times[times.shape[0]-max_samples:times.shape[0],]
+        data_array = np.transpose(times)
+        pos = np.arange(data_array.shape[1]) + (idx * off) + center
+        flierprops = dict(marker='o', markerfacecolor=colors[idx], markersize=1,
+                          linestyle='none', markeredgecolor=colors[idx])
+        bp = ax.boxplot(data_array, widths=width, patch_artist=True,
+                        positions=pos, manage_ticks=False, showfliers=True,
+                        flierprops=flierprops, meanline=True)
+        for element in ['boxes', 'whiskers', 'fliers', 'medians', 'caps']:
+            plt.setp(bp[element], color=colors[idx])
+        for patch in bp['boxes']:
+            patch.set(facecolor=fill_color)
+
+    if oput_fn is not None:
+        plt.savefig(oput_fn+'.png', bbox_inches='tight')
+
+    plt.set_title(title)
+    plt.xticks(range(max_samples))
+    # plt.xlabel()
+    # plt.ylabel()
+    plt.legend(custom_lines, labels, loc='upper right', prop={'size':7})
+    plt.show()
 
 # MAIN
 parser = argparse.ArgumentParser(description='DPHPC data processing utility.')
@@ -149,6 +213,7 @@ parser.add_argument('-pickled', dest='parser', action='store_const',
                     help='parse data from pickled object (default: parse file)')
 parser.add_argument('-title', dest='title',
                     default='Default Title', help='plot title')
+parser.add_argument('-costs', nargs=1, dest='costfn', help='todo')
 parser.add_argument('-p-out', dest='picklefn', nargs='?',
                     default=None, help='filename to store the parsed file as pickled object')
 parser.add_argument('-g-out', dest='graphfn', nargs='?',
@@ -159,13 +224,15 @@ parser.add_argument('-Y', dest='force', action='store_const',
 
 arg = parser.parse_args()
 maybe_warn(arg.force, 'Warning: Ouput files will be force overwritten.')
+
 data_sets = lmap(arg.parser, arg.files)
+# costs = arg.parser(arg.files)
 
 if arg.picklefn is not None and len(arg.files) == 1:
     store_as_pickle(data_sets[0], arg.picklefn, arg.force)
 
 # TODO add support for multiple graphs
-plot_init_settings()
-
-barplot(data_sets, arg.files, oput_fn=arg.graphfn, title=arg.title)
+# plot_init_settings()
+# barplot(data_sets, arg.files, oput_fn=arg.graphfn, title=arg.title)
 # boxplot(data_sets, arg.files, oput_fn=arg.graphfn, title=arg.title)
+# grouped_boxplot(data_sets, costs, arg.files, oput_fn=arg.graphfn, title=arg.title)
